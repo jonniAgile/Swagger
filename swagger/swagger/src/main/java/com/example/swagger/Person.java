78ef6ba0239511a096837d44994c1ac3fe2839a1
jonni <jonni.eduardo@agilethought.com>
2020-03-02 12:50:22 -0600

    Creacion de etiquetas.

--- swagger/swagger/src/main/java/com/example/swagger/Person.java
+++ swagger/swagger/src/main/java/com/example/swagger/Person.java
@@ -1,14 +1,22 @@
 package com.example.swagger;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+@ApiModel(description = "All details about the Person. ")
 public class Person {
 	
-	private String name, lastname;
+	@ApiModelProperty(notes = "The person first name")
+	private String name;
+	
+	@ApiModelProperty(notes = "The person last name")
+	private String lastname;
 	
 	public String getName() {
 		return name;
 	}
 	
-	public void setName (String name) {
+	public void setName(String name) {
 		this.name = name;
 	}
 	
--- swagger/swagger/src/main/java/com/example/swagger/controller/ServicePeople.java
+++ swagger/swagger/src/main/java/com/example/swagger/controller/ServicePeople.java
@@ -1,5 +1,4 @@
 package com.example.swagger.controller;
-
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
@@ -15,35 +14,46 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.swagger.Person;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+
 @RestController
+@Api(value = "People management system", description = "Register system of people")
 public class ServicePeople {
 	private Map<Integer,Person> people;
 	public ServicePeople() {
 		people = new HashMap<>();		
 	}
+	@ApiOperation(value = "Add new person")
 	@PutMapping("/put/{idPerson}")
-	public Person putPerson(@PathVariable int idPerson, @RequestBody Person person) {
+	public Person putPerson(@ApiParam(value = "Number of identification", required = true) 
+	@PathVariable int idPerson, @RequestBody Person person) {
 		people.put(idPerson, person);
 		return person;
 	}
+	@ApiOperation(value = "Get a person by Id")
 	@GetMapping("/get")
-	public Person getPerson(@RequestParam("idPerson") int idPerson ) {
+	public Person getPerson(@ApiParam(value = "Number of identification stored in database", required = true) 
+	@RequestParam("idPerson") int idPerson ) {
 		return people.get(idPerson);
 	}
+	@ApiOperation(value = "Update available person")
 	@PostMapping("/post")
 	public Person postPerson(@RequestBody Person idPerson) {
 		Random r = new Random();
 		people.put(r.nextInt(), idPerson);
 		return idPerson;
 	}
+	@ApiOperation(value = "Delete a person")
 	@DeleteMapping("/delete/{idPerson}")
-	public String deletePerson(@PathVariable int idPerson){
+	public String deletePerson(@ApiParam(value = "Number Id from which person will delete from database", required = true)
+	@PathVariable int idPerson){
 		try {
 			people.remove(idPerson);
 		} catch (Exception e) {
 			return "Error";
 		}
 		return "Successful";
-	}
-	
+	}	
 }
